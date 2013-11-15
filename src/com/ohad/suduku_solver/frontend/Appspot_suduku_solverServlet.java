058b0b035364971ad9d4859f174db7b6d1b4d0df
OhadR <ohadr.developer@gmail.com>
2013-11-15 23:38:40 +0200

    improve front-end servlet
    next version, that is using AJAX so the current page will be updated.

--- src/com/ohad/suduku_solver/frontend/Appspot_suduku_solverServlet.java
+++ src/com/ohad/suduku_solver/frontend/Appspot_suduku_solverServlet.java
@@ -1,98 +1,86 @@
-/*
-this is the first version - gets ALL indexes: 00, 01, 02 till 77 and fills the table,
-then try to solve it.
-after solving, it put it on the response, so the user sees a new generated HTML with the result.
-
-next version will be using AJAX so the current page will be updated.
-ohad, 22-Dec-2010.
-*/
-
-package com.ohad.frontend;
+package com.ohad.suduku_solver.frontend;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.http.*;
 
-import com.ohad.suduku_solver.SolutionItem;
-import com.ohad.suduku_solver.SudukuPuzzle;
-import com.ohad.suduku_solver.SudukuSolver;
-import static com.ohad.suduku_solver.SudukuPuzzle.TABLE_SIZE;
+import com.google.gson.Gson;
+import com.google.gson.JsonParseException;
+import com.ohad.suduku_solver.engine.SolutionItem;
+import com.ohad.suduku_solver.engine.SudukuPuzzle;
+import com.ohad.suduku_solver.engine.SudukuSolver;
 
 
 @SuppressWarnings("serial")
 public class Appspot_suduku_solverServlet extends HttpServlet 
 {
-	private int table[][] = new int[TABLE_SIZE][TABLE_SIZE];
-
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 	throws IOException 
 	{
-		resp.setContentType("text/plain");
+	    resp.setContentType("text/plain");
+
+	    PrintWriter writer = resp.getWriter();
+        ResultStatus status = new ResultStatus();
+
+	    Gson gson = new Gson();
+        String command = req.getParameter("command");
+	    String jsonedPuzzle = req.getParameter("puzzle");
+	    //if input contains something that is not int[][], we will get exception. so if user enters
+	    //things like letters, i want the server side to protect itself (doing it in the UI is nice
+	    //but if i have several UIs like several HTMLs, i dont want wach one of them to contains protection...
+	    try
+	    {
+	        String errMsg = "";
+	        int[][] inputPuzzle = gson.fromJson(jsonedPuzzle, int[][].class);
+
+	        SudukuPuzzle input = new SudukuPuzzle(inputPuzzle);
+	        SudukuSolver solver = new SudukuSolver();
+
+	        //we need to put TABLE_SIZE numbers, each one TABLE_SIZE times:
+	        SolutionItem puzzleToSolve = new SolutionItem(input, 0, -1);
+	        SolutionItem solved = null;
+	        if( command.equals( "solve" ))
+	        {
+	            solved = solver.solve(puzzleToSolve);
+	        }
+	        else if( command.equals( "gethint" ))
+	        {
+	            solved = solver.getHint(puzzleToSolve);
+	        }
+	        else
+	        {
+                errMsg = "illegal command from the javascript";
+	        }
+
+	        //Gson gson = new GsonBuilder().serializeNulls().create();
+	        gson = new Gson(); 
+	        
+	        if(solved == null) //could not solve or illegal command
+	        {
+	            errMsg = (errMsg == "") ? "ERROR: could not solve this fucking puzzle; took me " + solver.getNumSteps() + " steps" : errMsg;  
+	            status.setSuccess(false);
+	            status.setMessage( errMsg );
+	        }
+	        else
+	        {
+	            status.setSuccess(true);
+	            status.setMessage("solved puzzle in " + solver.getNumSteps() + " steps");
+	            //since i work with Json i get the table and transfer it as is:
+	            status.setPuzzle(solved.puzzle.getTable());
+	        }
+	    }
+	    catch(JsonParseException jpe)
+	    {
+            status.setSuccess(false);
+            status.setMessage("ERROR: The input must contains ONLY numbers");
+	    }
+
+
+
 		
-		PrintWriter writer = resp.getWriter();
-
-		try
-		{
-			for(int x = 0; x < TABLE_SIZE; ++x)
-			{
-				for(int y = 0; y < TABLE_SIZE; ++y)
-				{
-					String indexes = String.valueOf(x) + String.valueOf(y);
-					String value = req.getParameter(indexes);
-					if(value == "")
-					{
-						table[x][y] = 0;
-					}
-					else
-					{
-						int valueOf = Integer.valueOf(value);
-						if(valueOf < 0 || valueOf > 9)
-						{
-							throw new NumberFormatException();
-						}
-						table[x][y] = valueOf;
-					}
-				}
-			}
-		}
-		catch(NumberFormatException nfe)
-		{
-			writer.println("Please use only numbers, 0-9, and try again.");
-			return;
-		}
-
-
-
-//        for(int x = 0; x < TABLE_SIZE; ++x)
-//        {
-//            for(int y = 0; y < TABLE_SIZE; ++y)
-//            {
-//        		resp.getWriter().print( table[x][y] + " " );
-//            }
-//            resp.getWriter().println();
-//        }
-
-		SudukuPuzzle input = new SudukuPuzzle(table);
-		SudukuSolver solver = new SudukuSolver();
-
-		//we need to put TABLE_SIZE numers, each one TABLE_SIZE times:
-		SolutionItem puzzleToSolve = new SolutionItem(input, 1, 0, -1);
-		SolutionItem solved = solver.solve(puzzleToSolve);
-		if(solved == null)
-		{
-			writer.println("could not solve this fucking puzzle");
-		}
-		else
-		{
-			printPuzzle(writer, solved.puzzle);
-			writer.println("solved puzzle in " + solver.getNumSteps() + " steps" );
-		}
-	}
-
-	private void printPuzzle(PrintWriter printWriter, SudukuPuzzle puzzle) 
-	{
-		printWriter.println("the solved puzzle:");
-		puzzle.printPuzzle(printWriter);
+        String json = gson.toJson( status );
+        System.out.println( json );
+        writer.println( json );
 	}
 }
